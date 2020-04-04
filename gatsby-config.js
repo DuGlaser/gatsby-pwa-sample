1b986251bb0af2f1b645c47ed32c24570fc9077e
DuGlaser <duglaser0618@gmail.com>
2020-04-04 13:31:17 +0900

    :wrench:ADD manifest settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     // Add typescript stack into webpack
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "sample",
+        short_name: "sample",
+        start_url: "/?utm_source=homescreen",
+        // detail: https://web.dev/add-manifest/
+        background_color: "#fff", // app startup background
+        theme_color: "#d23d29", // browser tool ber color
+        display: "minimal-ui", // app style
+        icon: "static/icons/app.png",
+      },
+    },
   ],
-}
+};
